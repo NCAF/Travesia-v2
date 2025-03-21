7a84bb03a2dd53af352f3197b4fd8cf56797d77b
Illiyyin Akbar Ariyanto <illiyyin.akbar123@gmail.com>
2025-03-21 10:07:36 +0700

    Some changes in navbar.

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -12,7 +12,7 @@
             <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Destination</a>
+                        <a class="nav-link" href="{{ route('user.destination-list') }}">Destination</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Chat</a>
@@ -22,44 +22,54 @@
                     </li>
                 </ul>
                 {{-- ini untuk guest --}}
-                <!-- Tombol di Desktop -->
-                {{-- <button class="custom-btn-outline btn fw-bold mx-2 d-none d-lg-inline">Sign In</button>
-                <button class="custom-btn btn fw-bold mx-2 d-none d-lg-inline">Sign Up</button> --}}
+                @if (!Auth::check())
+                    <a href="{{ route('login') }}" class="custom-btn-outline btn fw-bold mx-2 d-none d-lg-inline">Sign In</a>
+                    <a href="{{ route('register') }}" class="custom-btn btn fw-bold mx-2 d-none d-lg-inline">Sign Up</a>
+                @endif
 
                 <!-- Teks di Mobile & Tablet -->
                 {{-- <ul class="navbar-nav d-lg-none">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Sign In</a>
+                        <a class="nav-link" href="{{ route('login') }}">Sign In</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Sign Up</a>
+                        <a class="nav-link" href="{{ route('register') }}">Sign Up</a>
                     </li>
                 </ul> --}}
 
+                @if (Auth::check())
+                    @if (Auth::user()->role == 'user')
+                        <a href="{{ route('user.dashboard') }}" class="btn btn-primary">Dashboard</a>
+                    @elseif (Auth::user()->role == 'driver')
+                        <a href="{{ route('driver.dashboard') }}" class="btn btn-primary">Dashboard</a>
+                    @endif
+                @endif
+
                 {{-- ini untuk user/driver --}}
                 <!-- User Profile Desktop -->
-                <div class="d-none d-lg-flex align-items-center dropdown">
-                    <div class="text-end me-2">
-                        <span class="fw-bold d-block">Muhammad Adib F.</span>
-                        <span class="text-muted small">User</span>
+                 @if (Auth::check())
+                    <div class="d-none d-lg-flex align-items-center dropdown">
+                        <div class="text-end me-2">
+                            <span class="fw-bold d-block">{{ Auth::user()->nama }}</span>
+                            <span class="text-muted small">{{ Auth::user()->role }}</span>
+                        </div>
+
+                        <!-- Dropdown Toggle -->
+                        <img src="{{ asset('icons/icon-img.svg') }}" alt="Profile Image"
+                            class="rounded-circle dropdown-toggle" width="32" height="32" id="profileDropdown"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+
+                        <!-- Dropdown Menu -->
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="profileDropdown">
+                            <li><a class="dropdown-item" href="{{ route('logout') }}">Logout</a></li>
+                        </ul>
                     </div>
-
-                    <!-- Dropdown Toggle -->
-                    <img src="{{ asset('icons/icon-img.svg') }}" alt="Profile Image"
-                        class="rounded-circle dropdown-toggle" width="32" height="32" id="profileDropdown"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-
-                    <!-- Dropdown Menu -->
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="profileDropdown">
-                        <li><a class="dropdown-item" href="#">Logout</a></li>
-                    </ul>
-                </div>
-
+                @endif
 
                 <!-- Teks di Mobile & Tablet -->
                 <ul class="navbar-nav d-lg-none">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Logout</a>
+                        <a class="nav-link" href="{{ route('logout') }}">Logout</a>
                     </li>
                 </ul>
 
