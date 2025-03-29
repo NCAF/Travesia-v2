81089e2ee1db1ad560c356af3d7cac4a2ddce63d
Illiyyin Akbar Ariyanto <illiyyin.akbar123@gmail.com>
2025-03-29 22:13:37 +0700

    Add controller image.

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -6,15 +6,15 @@ use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
-    public function uploadImage(Request $request){
-        $request->validate([
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-        ]);
-
-        $image = $request->file('image');
-        $imageName = time().'.'.$image->getClientOriginalExtension();
-        $image->move(public_path('images'), $imageName);
-        return response()->json(['success'=>$imageName]);
+    public function uploadImage($file){
+        // Check if file exists
+        if (!$file) {
+            return null;
+        }
+        
+        $imageName = time().'.'.$file->getClientOriginalExtension();
+        $file->move(public_path('images'), $imageName);
+        return $imageName;
     }
 
     public function viewImage($imageName){
